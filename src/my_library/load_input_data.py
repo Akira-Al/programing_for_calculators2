0e549f791f79d1990a7bb4992dc6471cfd3718a6
23chiba <chibataishi0526@g.ecc.u-tokyo.ac.jp>
2024-07-10 02:35:42 +0900

    dictionray1だけを考慮したものは正答率:0.6

--- src/my_library/load_input_data.py
+++ src/my_library/load_input_data.py
@@ -13,3 +13,16 @@ def load_raw_data(file_path = "../data/data.txt"):
     all_lines = f.read()
   all_lines_list = all_lines.strip().split("\n")
   return all_lines_list
+
+def load_raw_ans_data(file_path = "../data/data_ans.txt"):
+  with open(file_path, 'r', encoding="utf-8") as f:
+    all_lines = f.read()
+    all_lines_list = all_lines.strip().split("\n")
+    ans_list = [ 0 for _ in range(len(all_lines_list))]
+    for i in range(len(all_lines_list)):
+        line_list = all_lines_list[i].split(" ")
+        if line_list[0] == "p":
+            ans_list[i] = 1
+        elif line_list[0] == "n":
+            ans_list[i] = -1
+    return ans_list
--- src/predict_polarity.py
+++ src/predict_polarity.py
@@ -6,6 +6,7 @@ project_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model_path = os.path.join(project_root_path, "models", "best_model.pkl")
 preprocessed_data_path = os.path.join(project_root_path, "data", "data_preprocessed.pkl")
 data_path = os.path.join(project_root_path, "data", "data.txt")
+data_ans_path = os.path.join(project_root_path, "data", "data_ans.txt")
 
 with open(model_path, "rb") as f:
     predictor = pickle.load(f)
@@ -14,6 +15,15 @@ with open(preprocessed_data_path, "rb") as f:
 
 print(preprocessed_data[1:10])
 sentence_array = input_loader.load_raw_data(data_path)
+sentence_ans_array = input_loader.load_raw_ans_data(data_ans_path)
 predictions = predictor.predict(preprocessed_data)
+# result = [不正解数, 正解数]
+result = [0, 0]
 for i in range(len(predictions)):
-    print(sentence_array[i], predictions[i])
+    is_correct = 1 if predictions[i] == sentence_ans_array[i] else 0
+    result[is_correct] += 1
+    if not is_correct:
+        print(sentence_array[i])
+        print("結果:", predictions[i], "解答:", sentence_ans_array[i])
+
+print("正答率:", result[1] / (result[0] + result[1]))
